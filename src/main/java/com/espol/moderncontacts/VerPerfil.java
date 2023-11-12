f379b9b56d8282122ada630064017cc64c2695b6
erillope <ericklorenzolopez1@gmail.com>
2023-11-11 19:41:53 -0500

    Plantilla para editor de perfil

---
+++ src/main/java/com/espol/moderncontacts/VerPerfil.java
@@ -0,0 +1,43 @@
+
+package com.espol.moderncontacts;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class VerPerfil {
+
+    @FXML
+    private ImageView fotoPerfil;
+
+    @FXML
+    private Label nombrePerfil;
+
+    @FXML
+    private Label numeros;
+
+    @FXML
+    private Label emails;
+
+    @FXML
+    private Label driecciones;
+
+    @FXML
+    private Label fechas;
+
+    @FXML
+    private Label redes;
+
+    @FXML
+    void borrarPerfil(MouseEvent event) {
+
+    }
+
+    @FXML
+    void retroceder(MouseEvent event) {
+
+    }
+
+}
+
