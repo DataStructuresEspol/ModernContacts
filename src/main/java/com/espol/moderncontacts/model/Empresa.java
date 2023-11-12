a8b497659a67079e133ffa5e929db88080d49c85
erillope <ericklorenzolopez1@gmail.com>
2023-11-11 19:54:25 -0500

    adicion atributo tipo a contacto

--- src/main/java/com/espol/moderncontacts/model/Empresa.java
+++ src/main/java/com/espol/moderncontacts/model/Empresa.java
@@ -7,10 +7,10 @@ import java.util.Date;
 
 public class Empresa extends Contacto{
     private Date fechaAniversario;
-    private String tipo = "empresa";
 
     public Empresa(String nombre, String telefono, String codePais) {
         super(nombre, telefono, codePais);
+        tipo = "empresa";
     }
     
     public void setFechaAniversario(Date fecha){fechaAniversario = fecha;}
